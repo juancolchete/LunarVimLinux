6d270581fbae13036f96aefc2689a50e8a34d2c0
juancolchete <juanudkp@gmail.com>
2023-06-11 10:53:06 -0300

    chore: add troubles and linting

--- config.lua
+++ config.lua
@@ -1,6 +1,17 @@
-lvim.plugins = {
-  { "lunarvim/colorschemes" },
-  { "wakatime/vim-wakatime"}
+lvim.builtin.which_key.mappings["ç"] = {
+  name = "+Trouble",
+  l = {"<cmd>TroubleToggle<cr>","General panel"},
+  k = {"<cmd>TroubleToggle workspace_diagnostics<cr>","Workspace diagnostics"},
+  j = {"<cmd>TroubleToggle document_diagnostics<cr>","Document diagnosticas"},
+  h = {"<cmd>TroubleToggle loclist<cr>","Loclist"},
+  g = {"<cmd>TroubleToggle quickfix<cr>","Quickfix"},
+  f = {"<cmd>TroubleToggle lsp_references<cr>","LSP References"}
 }
 
-
+lvim.plugins = {
+  { "lunarvim/colorschemes" },
+  { "wakatime/vim-wakatime"},
+  { 'jose-elias-alvarez/typescript.nvim' },
+  { 'mg979/vim-visual-multi' },
+  { 'folke/trouble.nvim'}
+}
