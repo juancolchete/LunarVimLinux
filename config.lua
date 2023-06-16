191cb7586c45454562473939b440e13b9e7b48b7
juancolchete <juanudkp@gmail.com>
2023-06-16 17:32:54 -0300

    chore: add select all keybinding on ctrl a

--- config.lua
+++ config.lua
@@ -12,9 +12,12 @@ configs.solidity = {
   },
 }
 
+lvim.keys.normal_mode["<S-f>"] = ":Telescope live_grep<CR>"
 lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["<C-a>"] = "ggVG"
+
 lvim.builtin.which_key.mappings["ç"] = {
   name = "+Trouble",
   l = {"<cmd>TroubleToggle<cr>","General panel"},
